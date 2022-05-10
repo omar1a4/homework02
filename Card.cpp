d36f590955f9feb15ecef9469eca0d4df5afe257
layanqaddah <103147665+layanqaddah@users.noreply.github.com>
2022-05-10 22:20:11 +0300

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -6,6 +6,40 @@ Card::Card(CardType type, const CardStats& stats): m_effect(type), m_stats(stats
 {
 }
 
+void Card :: applyEncounter(Player& player) const
+{
+    if (m_effect == CardType::Battle)
+    {
+       if(player.getAttackStrength() >= m_stats.force)
+       {
+           player.levelUp();
+           player.addCoins(m_stats.loot);
+       }
+       else
+       {
+           player.damage(m_stats.hpLossOnDefeat);
+       }
+    }
+    if (m_effect == CardType ::Heal)
+    {
+        if(player.pay(m_stats.cost))
+        {
+            player.heal(m_stats.heal);
+        }
+    }
+    if (m_effect == CardType ::Buff)
+    {
+        if(player.pay(m_stats.cost))
+        {
+            player.heal(m_stats.buff);
+        }
+    }
+    if (m_effect == CardType ::Treasure)
+    {
+       player.addCoins(m_stats.loot);
+    }
+
+}
 
 void Card::printInfo() const
 {
